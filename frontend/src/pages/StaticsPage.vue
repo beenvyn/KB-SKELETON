297ed3c9410513b649840844db1880e90c6aab97
beenvyn <beenvyn5262@gmail.com>
2025-04-08 11:06:30 +0900

    chore : 기본 컴포넌트 & 페이지 세팅

---
+++ frontend/src/pages/StaticsPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>StaticsPage</h1>
+  </div>
+</template>
+<script setup></script>
+<style scoped></style>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "main",
+      component: () => import("@/pages/MainPage.vue"),
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: () => import("@/pages/SignupPage.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("@/pages/LoginPage.vue"),
+    },
+    {
+      path: "/record",
+      name: "record",
+      component: () => import("@/pages/RecordPage.vue"),
+    },
+    {
+      path: "/statics",
+      name: "statics",
+      component: () => import("@/pages/StaticsPage.vue"),
+    },
+    {
+      path: "/expense",
+      name: "expense",
+      component: () => import("@/pages/ExpensePage.vue"),
+    },
+    {
+      path: "/detail",
+      name: "detail",
+      component: () => import("@/pages/DetailPage.vue"),
+    },
+  ],
+});
+
+export default router;
